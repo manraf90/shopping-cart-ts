79402ac3b145268c057ec9b4e72c96f535460523
manraf90 <rafal.mankowski@smartive.app>
2025-12-24 16:27:24 +0100

    Fix deploy

--- src/pages/FirstGiftPage/FirstGiftPage.tsx
+++ src/pages/FirstGiftPage/FirstGiftPage.tsx
@@ -1,13 +1,9 @@
-import { useNavigate } from 'react-router-dom';
 import paris from '../../assets/paris.jpg';
 import parisDescription from '../../assets/paris_description.jpg';
-import ButtonBase from '../../components/ButtonBase/ButtonBase';
 
 import './FirstGiftPage.scss';
 
 const FirstGiftPage: React.FC = () => {
-    const navigate = useNavigate();
-
     return (
         <div className="first_gift_page">
             <h1>Merry Christmas !!! :)</h1>
