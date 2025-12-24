75355458ea5ad233bf28c1881c02dcaba464bd62
manraf90 <rafal.mankowski@smartive.app>
2025-12-24 16:25:19 +0100

    Fix deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
 
-import Header from './components/Header/Header';
-import HomePage from './pages/HomePage/HomePage';
 import ContactPage from './pages/ContactPage/ContactPage';
 // import ProductListPage from './pages/ProductsListPage/ProductListPage';
 import DinnerPage from './pages/DinnerPage/DinnerPage';
@@ -13,7 +11,6 @@ import './App.scss';
 const App: React.FC = () => {
     return (
         <>
-            {/* <Header /> */}
             <div className="container">
                 <Routes>
                     <Route path="/" element={<FirstGiftPage />} />
@@ -21,7 +18,6 @@ const App: React.FC = () => {
                     <Route path="/contact" element={<ContactPage />} />
                     <Route path="/dinner" element={<DinnerPage />} />
                     <Route path="/last-gift" element={<LastGiftPage />} />
-                    <Route path="/first-gift" element={<FirstGiftPage />} />
                 </Routes>
             </div>
         </>
